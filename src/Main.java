16a52c2d2086900c539838d69cad2cc575710dfc
HKHK999 <159094600+HKHK999@users.noreply.github.com>
2024-07-10 21:21:15 -0400

    -"second commit"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("KEK");
+            Thread.sleep(2000);
+        }
     }
+
 }
